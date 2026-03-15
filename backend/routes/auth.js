8b411caaeaed571ea6dafd623bf76afec0a03841
BATMAN <gauravgirkar194@gmail.com>
2026-03-15 21:22:13 +0530

    feat: add login route with JWT authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -24,6 +25,33 @@ router.post('/register', async (req,res)=>{
         }
     }catch(error){
             res.status(500).json({ message: "Server error", error: error.message });
+    }
+})
+
+router.post('/login', async (req,res)=>{
+    const{ email, password } = req.body;
+    try{
+        const foundUser = await User.findOne({email:email});
+        if(!foundUser){
+            console.log("user found");
+            return res.status(400).json({message:"Invalid credentials."});
+        }
+        const isMatch = await bcryptjs.compare(password, foundUser.password);
+        if(!isMatch){
+            console.log("pass matched found");
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        else{
+            const token=jwt.sign(
+                { id: foundUser._id },
+                process.env.JWT_SECRET,
+                { expiresIn: '5d' },
+            )
+            res.status(200).json({ token, message: "Login successful" });
+        }
+    }
+    catch(error){
+        res.status(500).json({message: "Server down."});
     }
 })
 
